b3f42903718e191d44f3c2f8da505744dae05f19
KYCoraxxx <2631197015@qq.com>
2022-12-03 18:59:58 +0800

    adjust project structure

--- ScarboroughFair/src/main/resources/static/js/index.js
+++ ScarboroughFair/src/main/resources/static/js/index.js
@@ -1,32 +1,3 @@
-var avatorScaler = function (size){
-    $(".avator img").css("width", size);
-    $(".avator img").css("height", size);
-    if(size === "100px")
-        appendDropDown();
-    else
-        deleteDropDown();
-}
-var deleteDropDown = function (){
-    $(".userBox").css("height", "70px");
-    $(".dropDown").remove();
-}
-var appendDropDown = function (){
-    var dropDown = $("<div class='dropDown'></div>");
-    var userBox = $(".userBox");
-    userBox.css("height", "410px");
-    userBox.append(dropDown);
-}
-var changeSearch = function (state){
-    if(state === 0)
-        $(".searchBar img").css("background-color", "#00000024");
-    else if(state === 1)
-        $(".searchBar img").css("background-color", "#00000000");
-    else if(state === 2)
-        $(".searchBar").css("background-color", "#0000001a");
-    else
-        $(".searchBar").css("background-color", "#0000000a");
-}
-
 var items = $(".loopItem");
 var points = $(".loopPoint");
 var left = $("#leftBtn");
