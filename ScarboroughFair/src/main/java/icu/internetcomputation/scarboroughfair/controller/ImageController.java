b72b3ae1d72516e0435c938a61878f29f0a4d7db
Begonia <2239849100@qq.com>
2022-12-04 18:51:29 +0800

    uploadImg Controller in local accomplished

---
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/ImageController.java
@@ -0,0 +1,38 @@
+package icu.internetcomputation.scarboroughfair.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import icu.internetcomputation.scarboroughfair.entity.Message;
+import java.io.File;
+import java.util.UUID;
+
+@RestController
+public class ImageController {
+
+    @PostMapping("/upload")
+    public Object upload(MultipartFile fileUpload){
+
+        String baseDir = "/testImageBalabala/hha";//相对路径 以项目路径为准
+
+        String fileName = fileUpload.getOriginalFilename();
+
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+
+        fileName = UUID.randomUUID()+suffixName;
+        try {
+            File f=new File(baseDir, fileName);
+
+            if (!f.getParentFile().exists()) {   //create parent file
+                f.getParentFile().mkdirs();
+            }
+
+            fileUpload.transferTo(f.getAbsoluteFile());
+
+            return new Message(true,"上传成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Message(false,"上传失败");
+        }
+    }
+}
