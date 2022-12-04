fc596c4bd680d84b5dfcae2db9c94ff04dcd4878
Begonia <2239849100@qq.com>
2022-12-04 23:12:05 +0800

    little change

--- ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/ScarboroughFairController.java
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/ScarboroughFairController.java
@@ -3,7 +3,7 @@ package icu.internetcomputation.scarboroughfair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import icu.internetcomputation.scarboroughfair.entity.Message;
 import icu.internetcomputation.scarboroughfair.service.UserService;
 import org.springframework.ui.Model;
@@ -17,10 +17,14 @@ public class ScarboroughFairController {
         return "login";
     }
 
-    @GetMapping("/index")
+    @RequestMapping(path="/index",method = RequestMethod.POST)
     public String homepage(Model model){
         return "index";
     }
 
+    @RequestMapping(path="/detail",method = RequestMethod.GET)
+    public String detailpage(Model model){
+        return "detail";
+    }
    
 }
