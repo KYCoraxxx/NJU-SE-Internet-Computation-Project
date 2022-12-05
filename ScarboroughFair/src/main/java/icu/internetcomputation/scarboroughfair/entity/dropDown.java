161688b5a65c0951fe04ba53c2db4fcd1360ba9a
Begonia <2239849100@qq.com>
2022-12-05 18:36:00 +0800

    add dropdown Entity and corresponding method

---
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/entity/dropDown.java
@@ -0,0 +1,14 @@
+package icu.internetcomputation.scarboroughfair.entity;
+
+public class dropDown {
+    String userName=null;
+    int userId=0;
+    String avatorUrl=null;
+    
+    public dropDown(String name,int id,String avator)
+    {
+        this.userName=name;
+        this.userId=id;
+        this.avatorUrl=avator;
+    }
+}
