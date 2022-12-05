105b323bc159d0621f6c8d393dbadbf0759a42d5
Logician <936367005@qq.com>
2022-12-05 22:01:21 +0800

    completed detail page

--- ScarboroughFair/src/main/resources/static/js/detail.js
+++ ScarboroughFair/src/main/resources/static/js/detail.js
@@ -1,6 +1,22 @@
-var changeProviderLink = function (state){
+var changeProviderName = function (state){
     if (state === 0)
-        $(".goodProviderLink").css("color", "#000000");
+        $(".providerName").css("color", "#000000");
     else if (state === 1)
-        $(".goodProviderLink").css("color", "lightskyblue");
+        $(".providerName").css("color", "lightskyblue");
+}
+
+var showDetailContent = function(type){
+    $(".detailContent").remove();
+    var detailContent = $("<div class = 'detailContent'></div>")
+    var goodDetail = $(".goodDetail")
+    goodDetail.append(detailContent);
+    if (type === 1){
+        detailContent.append($("<div class = 'article-subtitle'>原产地：null</div>"));
+        detailContent.append($("<div class = 'article-subtitle'>保质期：null</div>"));
+        detailContent.append($("<div class = 'article-subtitle'>用途：null</div>"));
+    }
+    else if(type === 2){
+        detailContent.append($("<img src = '../static/img/demo5.jpg'/>"));
+        detailContent.append($("<img src = '../static/img/demo5.jpg'/>"));
+    }
 }
