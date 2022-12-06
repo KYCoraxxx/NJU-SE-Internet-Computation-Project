54539524025c4721ad30c76ac6693e1e930023b8
Begonia <2239849100@qq.com>
2022-12-06 23:29:34 +0800

    virtual img url

--- ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/ImageController.java
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/ImageController.java
@@ -4,19 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.text.SimpleDateFormat;
 import icu.internetcomputation.scarboroughfair.FileUtil;
 import icu.internetcomputation.scarboroughfair.entity.Message;
 import java.io.File;
 import java.util.UUID;
-
+import org.springframework.beans.factory.annotation.Value;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Controller
 @CrossOrigin
 public class ImageController {
 
     // private String[] uploadImageTypes = { "png", "jpg", "jpeg", "gif"};
+
+    @Value("${file.uploadFolder}")
+    private String realBasePath;
+    @Value("${file.accessPath}")
+    private String accessPath;
 
     @RequestMapping(path="/tmpupload",method = RequestMethod.GET)
     public String tmpupload(Model model){
@@ -44,27 +55,66 @@ public class ImageController {
         //     return new Message(false, "请选择格式正确的图片", null);
         // }
 
-        String UploadPath = "F:\\Img\\"; // 本地路径
+
+
+
+
+
+        //获取随机文件名
         String fileName = fileUpload.getOriginalFilename();
-        // String suffixName = fileName.substring(fileName.lastIndexOf("."));
-        String destFileName = UploadPath + fileName;
-        // fileName = UUID.randomUUID()+suffixName;
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        fileName = UUID.randomUUID()+suffixName;
+        //获取日期
+        Date todayDate = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String today = dateFormat.format(todayDate);
+        
+        // 域名访问的相对路径目录（通过浏览器访问的链接-虚拟路径）
+        String saveToPath = accessPath + today + "/";
+        // 真实路径，实际储存的目录
+        String realPath = realBasePath + today + "/";
+        // 储存文件的物理路径，使用本地路径储存
+        String filepath = realPath + fileName;
         try {
+            File f = new File(filepath);
             // File f=new File(UploadPath);
-            FileUtil.uploadFile(fileUpload.getBytes(), UploadPath, fileName);
+            // FileUtil.uploadFile(fileUpload.getBytes(), UploadPath, fileName);
             
-            // File targetfile = new File(destFileName);
-            // if (!targetfile.getParentFile().exists()) {   //create parent file
-            //     targetfile.getParentFile().mkdirs();
-            // }
+            File targetfile = new File(filepath);
+            if (!targetfile.getParentFile().exists()) {   //create parent file
+                targetfile.getParentFile().mkdirs();
+            }
 
             // fileUpload.transferTo(targetfile);
-            // fileUpload.transferTo(f.getAbsoluteFile());
+            fileUpload.transferTo(f.getAbsoluteFile());
 
-            return new Message(true,"上传成功", destFileName);
+            return new Message(true,"上传成功", saveToPath+fileName);
         } catch (Exception e) {
             e.printStackTrace();
             return new Message(false,"上传失败",null);
         }
     }
+
+
+
+
+
+@Configuration
+public class UploadConfig implements WebMvcConfigurer{
+
+  @Value("${file.staticAccessPath}")
+  private String staticAccessPath;
+
+  @Value("${file.uploadFolder}")
+  private String uploadFolder;
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) 
+  {
+   registry.addResourceHandler(staticAccessPath).addResourceLocations("file:" + uploadFolder);
+  }
+
+}
+
+
 }
