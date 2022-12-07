7b73cb816cbd772f7e32149ed9017cd0963b7f53
TTHA <626661840@qq.com>
2022-12-07 12:25:44 +0800

    change imgcontroller into Uploadcontroller

--- ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/UploadController.java
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/UploadController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.text.SimpleDateFormat;
 import icu.internetcomputation.scarboroughfair.entity.Message;
+import icu.internetcomputation.scarboroughfair.service.GoodService;
+
 import java.io.File;
 import java.util.UUID;
+
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Value;
-import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,7 +22,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
 @CrossOrigin
-public class ImageController {
+public class UploadController {
+
+    @Resource
+    private GoodService goodService;
 
 
     @Value("${file.uploadFolder}")
@@ -31,14 +37,31 @@ public class ImageController {
     public String tmpupload(Model model){
         return "tmpupload";
     }
+    // 测试接口
 
+
+    // original
     @PostMapping(path="/upload")
     @ResponseBody
     public Message upload(@RequestParam(value = "file") MultipartFile fileUpload, Model model){
-        return Imgupload(fileUpload);
+        return OriginalImgupload(fileUpload);
     }
 
-    public Message Imgupload(MultipartFile fileUpload){
+    @PostMapping(path="/GoodUpload")
+    @ResponseBody
+    public Message GoodUpload(@RequestParam(value = "file") MultipartFile fileUpload, 
+    @RequestParam(value = "name") String name, @RequestParam(value = "price") String price,
+    @RequestParam(value = "description") String description,Model model){
+        String GoodUrl = Imgupload(fileUpload);
+        if(GoodUrl == null){
+            return new Message(false, "图片好像上传失败了w(ﾟДﾟ)w");
+        }
+        return goodService.addGood(name, Float.valueOf(price), GoodUrl, description);
+    }
+
+    
+
+    public String Imgupload(MultipartFile fileUpload){
         String fileName = fileUpload.getOriginalFilename();
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
         fileName = UUID.randomUUID()+suffixName;
@@ -61,13 +84,45 @@ public class ImageController {
 
             fileUpload.transferTo(f.getAbsoluteFile());
 
-            return new Message(true,"上传成功", saveToPath+fileName);
+            return saveToPath+fileName;
         } catch (Exception e) {
             e.printStackTrace();
-            return new Message(false,"上传失败",null);
+            return null;
         }
     }
 
+    public Message OriginalImgupload(MultipartFile fileUpload){
+        String fileName = fileUpload.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        fileName = UUID.randomUUID()+suffixName;
+        //获取日期
+        Date todayDate = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String today = dateFormat.format(todayDate);
+        //域名访问的相对路径目录，通过浏览器访问的链接（虚拟路径）
+        String saveToPath = accessPath + today + "/";
+        // 真实的路径，实际储存的目录
+        String realPath = realBasePath + today + "/";
+        //储存文件的物理路径，建立在本地
+        String filepath = realPath + fileName;
+        try {
+            File f = new File(filepath);
+            
+            if (!f.getParentFile().exists()) {   //create parent file
+                f.getParentFile().mkdirs();
+            }
+
+            fileUpload.transferTo(f.getAbsoluteFile());
+
+            return new Message(true,"好耶，图片上传成功ヽ(✿ﾟ▽ﾟ)/", saveToPath+fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Message(false,"图片好像上传失败了w(ﾟДﾟ)w",null);
+        }
+    }
+
+
+    // 地址映射配置
     @Configuration
     public class UploadConfig implements WebMvcConfigurer{
         @Value("${file.staticAccessPath}")
