b167f1227729e69a57f057e64c67a46eec7aaf29
Logician <936367005@qq.com>
2022-12-08 17:44:06 +0800

    completed upload page, without valid test

--- ScarboroughFair/src/main/resources/static/js/upload.js
+++ ScarboroughFair/src/main/resources/static/js/upload.js
@@ -5,6 +5,8 @@ var postUploadInfo = function(){
     var tag = $("input[name='tag']");
     var price = $("input[name='price']");
     var description = $("input[name='description']");
+    var cover = $("#upl_cover")[0].files[0];
+    var pic = $("#upl_pic")[0].files[0];
     if (name[0].value === ""){
         if (upl_error !== undefined)
             upl_error.remove();
@@ -41,6 +43,24 @@ var postUploadInfo = function(){
         price.css("border-color", "mediumpurple");
         description.css("border-color", "red");
     }
+    else if (cover === null){
+        if (upl_error !== undefined)
+            upl_error.remove();
+        upl_error = $('<span>这篇大地的商品并非不需要被展示</span>');
+        name.css("border-color", "mediumpurple");
+        tag.css("border-color", "mediumpurple");
+        price.css("border-color", "mediumpurple");
+        description.css("border-color", "mediumpurple");
+    }
+    else if (pic === null){
+        if (upl_error !== undefined)
+            upl_error.remove();
+        upl_error = $('<span>这篇大地的商品并非不需要被展示</span>');
+        name.css("border-color", "mediumpurple");
+        tag.css("border-color", "mediumpurple");
+        price.css("border-color", "mediumpurple");
+        description.css("border-color", "mediumpurple");
+    }
     else {
         if (upl_error !== undefined){
             upl_error.remove();
@@ -56,6 +76,54 @@ var postUploadInfo = function(){
         upl_error.attr("id", "error");
         $(".uploadBar").append(upl_error);
     }
+    else{
+        uploadGoodInfo();
+    }
 }
 
-//todo: 获取封面和图片
+var displayNewCover = function(node){
+    var reader = new FileReader();
+    var file = node.files[0];
+    reader.readAsDataURL(file);
+    reader.onload = function (){
+        $("#cover").prop("src", this.result);
+    }
+}
+var displayNewPic = function(node){
+    var reader = new FileReader();
+    var file = node.files[0];
+    reader.readAsDataURL(file);
+    reader.onload = function (){
+        $("#pic").prop("src", this.result);
+    }
+}
+var uploadGoodInfo = function (){
+    var formData = new FormData();
+    var name = $("input[name='name']")[0];
+    var tag = $("input[name='tag']")[0];
+    var price = $("input[name='price']")[0];
+    var description = $("input[name='description']")[0];
+    var cover = $("#upl_cover")[0].files[0];
+    var pic = $("#upl_pic")[0].files[0];
+    formData.append("cover", cover);
+    formData.append("pic", pic);
+    formData.append("name", name.value);
+    formData.append("tag", tag.value);
+    formData.append("price", price.value);
+    formData.append("description", description.value);
+    $.ajax({
+        type: "POST",
+        url: server + "", //todo: Good upload url
+        data: formData,
+        async: false,
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (data){
+            if(data.isSucceed) {
+                alert(data.message);
+                window.location.replace("/index");
+            }
+        }
+    });
+}
