ccdab0be1973c56018e3a737042c46b803b9ae82
TTHA <626661840@qq.com>
2022-12-14 23:14:13 +0800

    addPagecontroller

--- ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/ScarboroughFairController.java
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/ScarboroughFairController.java
@@ -11,28 +11,37 @@ import org.springframework.ui.Model;
 @Controller
 public class ScarboroughFairController {
 
-    @GetMapping("/login")
-    public String login(Model model) {
-        return "login";
+    /**
+     * 去到page.html页面
+     * @return
+     */
+    @RequestMapping("/{page}")
+    public String toPage(@PathVariable String page) {
+        return page;
     }
+    
+    // @GetMapping("/login")
+    // public String login(Model model) {
+    //     return "login";
+    // }
 
-    @RequestMapping(path="/index",method = RequestMethod.GET)
-    public String homepage(Model model){
-        return "index";
-    }
+    // @RequestMapping(path="/index",method = RequestMethod.GET)
+    // public String homepage(Model model){
+    //     return "index";
+    // }
 
-    @GetMapping("/forum")
-    public String forum(Model model){ return "forum"; }
+    // @GetMapping("/forum")
+    // public String forum(Model model){ return "forum"; }
 
-    @GetMapping("/upload")
-    public String upload(Model model){ return "upload"; }
+    // @GetMapping("/upload")
+    // public String upload(Model model){ return "upload"; }
 
-    @GetMapping("/detail")
-    public String detail(Model model){ return "detail"; }
+    // @GetMapping("/detail")
+    // public String detail(Model model){ return "detail"; }
 
-    @GetMapping("/undone")
-    public String undone(Model model){ return "undone"; }
+    // @GetMapping("/undone")
+    // public String undone(Model model){ return "undone"; }
 
-    @GetMapping("/usercenter")
-    public String usercenter(Model model){ return "usercenter"; }
+    // @GetMapping("/usercenter")
+    // public String usercenter(Model model){ return "usercenter"; }
 }
