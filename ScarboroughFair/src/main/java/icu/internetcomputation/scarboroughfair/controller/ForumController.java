a4b8b26174a969ae955d00ec12512196ac580ff3
TTHA <626661840@qq.com>
2022-12-15 00:45:43 +0800

    add something in forumcontroller and forumservice

--- ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/ForumController.java
+++ ScarboroughFair/src/main/java/icu/internetcomputation/scarboroughfair/controller/ForumController.java
@@ -1,5 +1,19 @@
 package icu.internetcomputation.scarboroughfair.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import icu.internetcomputation.scarboroughfair.entity.Message;
+
+@Controller
+@RequestMapping(path = "/ForumService", method = RequestMethod.POST, produces = "application/json")
 public class ForumController {
+    @Resource
+    private ForumController forumController;
+
     
 }
